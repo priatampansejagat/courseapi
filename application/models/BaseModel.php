79d4c579ac40b1d9912ae5dedeaa23e37d105b7e
Tatag Winaryo <aku4layy@gmail.com>
2020-03-21 20:09:25 +0700

    database

--- application/models/BaseModel.php
+++ application/models/BaseModel.php
@@ -1,28 +1,28 @@
 <?php
 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+// if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class BaseModel extends CI_Model {
+// class BaseModel extends CI_Model {
 
-    public function __construct(){
-            parent::__construct();
-            $this->load->database();
-            $this->default = $this->load->database('default', TRUE);
-    }
+//     public function __construct(){
+//             parent::__construct();
+//             $this->load->database();
+//             $this->default = $this->load->database('default', TRUE);
+//     }
 
 
 
-    public function test(){
+//     public function test(){
 
-        $this->default->select('*')->from('menu');
-        $query = $this->default->get();
+//         $this->default->select('*')->from('menu');
+//         $query = $this->default->get();
         
-        return $query->row_array();
+//         return $query->row_array();
 
-    }
+//     }
 
 
-}
+// }
 
 
 ?>
