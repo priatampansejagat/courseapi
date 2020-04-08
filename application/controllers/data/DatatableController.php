c6a28e53c0dc4bd838efff80e6e8e64d2b59309e
Tatag Winaryo <aku4layy@gmail.com>
2020-04-08 16:34:32 +0700

    debug datatable

--- application/controllers/data/DatatableController.php
+++ application/controllers/data/DatatableController.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 // include('fungsi.php');
 
-class RegisterController extends CI_Controller
+class DatatableController extends CI_Controller
 {
 
 	public function __construct()
