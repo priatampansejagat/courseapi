352c24186b4c8476921c127e82627806398bac72
Tatag Winaryo <aku4layy@gmail.com>
2020-04-11 10:22:07 +0700

    debug course regis

--- application/controllers/CourseController.php
+++ application/controllers/CourseController.php
@@ -50,63 +50,70 @@ class CourseController extends CI_Controller
 
 		// load data user
 		$userCond = array('id' => $user_id,'role_id' => AS_STUDENT);
-		$user_data = $this->BasicQuery->selectAllResult('user',$userCond);
+		$userCount = $this->BasicQuery->countAllResult('user',$userCond);
+		if ($userCount != 0) {
+			$user_data = $this->BasicQuery->selectAllResult('user',$userCond);
 
-		// load data course
-		$courseCond = array('id' => $course_id);
-		$course_data = $this->BasicQuery->selectAllResult('course',$courseCond);
+			// load data course
+			$courseCond = array('id' => $course_id);
+			$course_data = $this->BasicQuery->selectAllResult('course',$courseCond);
 
-		// generate payment id
-		$payment_id = 'pay_'.date('Ymdhisa');
+			// generate payment id
+			$payment_id = 'pay_'.date('Ymdhisa');
 
-		// generate course member id
-		$member_id = 'coursemember_'.date('Ymdhisa');
+			// generate course member id
+			$member_id = 'coursemember_'.date('Ymdhisa');
 
 
-		// process ===========================================
-		// cek payment sudah ada apa belum
-		$payCond = array('id_user' => $user_id, 'course_id' => $course_id);
-		$payCount = $this->BasicQuery->countAllResult('payment',$payCond);
-		if ($payCount == 0) {
+			// process ===========================================
+			// cek payment sudah ada apa belum
+			$payCond = array('id_user' => $user_id, 'course_id' => $course_id);
+			$payCount = $this->BasicQuery->countAllResult('payment',$payCond);
+			if ($payCount == 0) {
 
-			// create payment
-			$payment_data=array(
-									'id'				=> $payment_id,
-									'id_user'			=> $user_id,
-									'nominal'			=> $course_data['price'],
-									'pay_nominal'		=> 0,
-									'proof_of_payment'	=> '#',
-									'pay_for'			=> 'course',
-									'course_id'			=> $course_id,
-									'status'			=> 0
+				// create payment
+				$payment_data=array(
+										'id'				=> $payment_id,
+										'id_user'			=> $user_id,
+										'nominal'			=> $course_data['price'],
+										'pay_nominal'		=> 0,
+										'proof_of_payment'	=> '#',
+										'pay_for'			=> 'course',
+										'course_id'			=> $course_id,
+										'status'			=> 0
 
-			);
-
-			$payment_insert = $this->BasicQuery->insert('payment', $payment_data);
-
-			if ($payment_insert == true) {
-				
-				// registering course member
-				$course_member_data = array(
-												'id'				=> $member_id,
-												'course_id'			=> $course_id,
-												'user_id'			=> $user_id,
-												'finished_chapter'	=> '',
-												'payment_id'		=> $payment_id
 				);
 
-				$member_insert = $this->BasicQuery->insert('course_member', $course_member_data);
+				$payment_insert = $this->BasicQuery->insert('payment', $payment_data);
 
-				if ($member_insert == true) {
+				if ($payment_insert == true) {
 					
-					// return data
-					$data_return = array(
-											'nominal' 		=> $payment_data['nominal'],
-											'trans_code'	=> $payment_data['id'],
-											'course'		=> $course_data
+					// registering course member
+					$course_member_data = array(
+													'id'				=> $member_id,
+													'course_id'			=> $course_id,
+													'user_id'			=> $user_id,
+													'finished_chapter'	=> '',
+													'payment_id'		=> $payment_id
 					);
-					$JSON_return = $this->GlobalFunction->return_JSON_success("Success to register",$data_return);
-					echo $JSON_return;
+
+					$member_insert = $this->BasicQuery->insert('course_member', $course_member_data);
+
+					if ($member_insert == true) {
+						
+						// return data
+						$data_return = array(
+												'nominal' 		=> $payment_data['nominal'],
+												'trans_code'	=> $payment_data['id'],
+												'course'		=> $course_data
+						);
+						$JSON_return = $this->GlobalFunction->return_JSON_success("Success to register",$data_return);
+						echo $JSON_return;
+
+					}else{
+						$JSON_return = $this->GlobalFunction->return_JSON_failed("Failed to register",$payment_data);
+						echo $JSON_return;
+					}
 
 				}else{
 					$JSON_return = $this->GlobalFunction->return_JSON_failed("Failed to register",$payment_data);
@@ -114,15 +121,15 @@ class CourseController extends CI_Controller
 				}
 
 			}else{
-				$JSON_return = $this->GlobalFunction->return_JSON_failed("Failed to register",$payment_data);
+				$payment_data = $this->BasicQuery->selectAll('payment',$payCond);
+				$JSON_return = $this->GlobalFunction->return_JSON_failed("Menunggu pembayaran",$payment_data);
 				echo $JSON_return;
 			}
-
 		}else{
-			$payment_data = $this->BasicQuery->selectAll('payment',$payCond);
-			$JSON_return = $this->GlobalFunction->return_JSON_failed("Menunggu pembayaran",$payment_data);
+			$JSON_return = $this->GlobalFunction->return_JSON_failed("User tidak terdaftar");
 			echo $JSON_return;
 		}
+		
 
 
 	}
