3af1d87067f1a4620d55f9ff273c296fae40e90b
Tatag Winaryo <aku4layy@gmail.com>
2020-04-15 11:15:37 +0700

    debug 4
    add header GET

--- application/controllers/CourseController.php
+++ application/controllers/CourseController.php
@@ -85,60 +85,60 @@ class CourseController extends CI_Controller
 		// header('content-type: application/json; charset=utf-8');
 		$this->globalfunction->header_CORS();
 
-		mkdir('./uploads/courses/temp/', 0777, true);
-		// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		// mkdir('./uploads/courses/temp/', 0777, true);
+		if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-		//     if(!(isset($_POST['resumableIdentifier']) && trim($_POST['resumableIdentifier'])!='')){
-		//         $_POST['resumableIdentifier']='';
-		//     }
-		//     $temp_dir = './uploads/courses/temp/'.$_POST['resumableIdentifier'];
+		    if(!(isset($_GET['resumableIdentifier']) && trim($_GET['resumableIdentifier'])!='')){
+		        $_GET['resumableIdentifier']='';
+		    }
+		    $temp_dir = './uploads/courses/temp/'.$_GET['resumableIdentifier'];
 
-		//     if(!(isset($_POST['resumableFilename']) && trim($_POST['resumableFilename'])!='')){
-		//         $_POST['resumableFilename']='';
-		//     }
-		//     if(!(isset($_POST['resumableChunkNumber']) && trim($_POST['resumableChunkNumber'])!='')){
-		//         $_POST['resumableChunkNumber']='';
-		//     }
-		//     $chunk_file = $temp_dir.'/'.$_POST['resumableFilename'].'.part'.$_POST['resumableChunkNumber'];
+		    if(!(isset($_GET['resumableFilename']) && trim($_GET['resumableFilename'])!='')){
+		        $_GET['resumableFilename']='';
+		    }
+		    if(!(isset($_GET['resumableChunkNumber']) && trim($_GET['resumableChunkNumber'])!='')){
+		        $_GET['resumableChunkNumber']='';
+		    }
+		    $chunk_file = $temp_dir.'/'.$_GET['resumableFilename'].'.part'.$_GET['resumableChunkNumber'];
 		    
-		//     if (file_exists($chunk_file)) {
-		//          header("HTTP/1.0 200 Ok");
-		//        } else {
-		//          header("HTTP/1.0 404 Not Found");
-		//        }
-		// }
+		    if (file_exists($chunk_file)) {
+		         header("HTTP/1.0 200 Ok");
+		       } else {
+		         header("HTTP/1.0 404 Not Found");
+		       }
+		}
 
 		// loop through files and move the chunks to a temporarily created directory
-		// if (!empty($_FILES)){
-		// 	foreach ($_FILES as $file) {
+		if (!empty($_FILES)){
+			foreach ($_FILES as $file) {
 				
-		// 	    // check the error status
-		// 	    if ($file['error'] != 0) {
-		// 	        // $this->globalfunction->_log('error '.$file['error'].' in file '.$_POST['resumableFilename']);
-		// 	        continue;
-		// 	    }
+			    // check the error status
+			    if ($file['error'] != 0) {
+			        // $this->globalfunction->_log('error '.$file['error'].' in file '.$_POST['resumableFilename']);
+			        continue;
+			    }
 
-		// 	    // init the destination file (format <filename.ext>.part<#chunk>
-		// 	    // the file is stored in a temporary directory
-		// 	    if(isset($_POST['resumableIdentifier']) && trim($_POST['resumableIdentifier'])!=''){
-		// 	        $temp_dir = './uploads/courses/temp/'.$_POST['resumableIdentifier'];
-		// 	    }
-		// 	    $dest_file = $temp_dir.'/'.$_POST['resumableFilename'].'.part'.$_POST['resumableChunkNumber'];
+			    // init the destination file (format <filename.ext>.part<#chunk>
+			    // the file is stored in a temporary directory
+			    if(isset($_POST['resumableIdentifier']) && trim($_POST['resumableIdentifier'])!=''){
+			        $temp_dir = './uploads/courses/temp/'.$_POST['resumableIdentifier'];
+			    }
+			    $dest_file = $temp_dir.'/'.$_POST['resumableFilename'].'.part'.$_POST['resumableChunkNumber'];
 
-		// 	    // create the temporary directory
-		// 	    if (!is_dir($temp_dir)) {
-		// 	        mkdir($temp_dir, 0777, true);
-		// 	    }
+			    // create the temporary directory
+			    if (!is_dir($temp_dir)) {
+			        mkdir($temp_dir, 0777, true);
+			    }
 
-		// 	    // move the temporary file
-		// 	    if (!move_uploaded_file($file['tmp_name'], $dest_file)) {
-		// 	        // $this->globalfunction->_log('Error saving (move_uploaded_file) chunk '.$_POST['resumableChunkNumber'].' for file '.$_POST['resumableFilename']);
-		// 	    } else {
-		// 	        // check if all the parts present, and create the final destination file
-		// 	        $this->globalfunction->createFileFromChunks($temp_dir, $_POST['resumableFilename'],$_POST['resumableChunkSize'], $_POST['resumableTotalSize'],$_POST['resumableTotalChunks']);
-		// 	    }
-		// 	}    
-		// }
+			    // move the temporary file
+			    if (!move_uploaded_file($file['tmp_name'], $dest_file)) {
+			        // $this->globalfunction->_log('Error saving (move_uploaded_file) chunk '.$_POST['resumableChunkNumber'].' for file '.$_POST['resumableFilename']);
+			    } else {
+			        // check if all the parts present, and create the final destination file
+			        $this->globalfunction->createFileFromChunks($temp_dir, $_POST['resumableFilename'],$_POST['resumableChunkSize'], $_POST['resumableTotalSize'],$_POST['resumableTotalChunks']);
+			    }
+			}    
+		}
 	}
 
 	public function registration(){
