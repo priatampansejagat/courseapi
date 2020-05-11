1e2d50fdb5e200d243316b0b775a746cd984cbac
Tatag Winaryo <aku4layy@gmail.com>
2020-05-11 21:46:43 +0700

    on air mode

--- application/config/config.php
+++ application/config/config.php
@@ -24,12 +24,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://api.research-academy.org/';
-$config['base_url'] = 'http://temporaryapi.rumahpeneleh.or.id/';
+// $config['base_url'] = 'http://temporaryapi.rumahpeneleh.or.id/';
 // $config['base_url'] = 'http://localhost/courseapi/';
 
-// $http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
-// $newurl = str_replace("index.php", "", $_SERVER['SCRIPT_NAME']);
-// $config['base_url']    = "$http" . $_SERVER['SERVER_NAME'] . "" . $newurl;
+$http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
+$newurl = str_replace("index.php", "", $_SERVER['SCRIPT_NAME']);
+$config['base_url']    = "$http" . $_SERVER['SERVER_NAME'] . "" . $newurl;
 
 /*
 |--------------------------------------------------------------------------
