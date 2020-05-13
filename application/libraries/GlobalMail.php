3399abf7391689d84143cd4b4647c809a2b187d7
Tatag Winaryo <aku4layy@gmail.com>
2020-05-13 19:19:18 +0700

    host fixing

--- application/libraries/GlobalMail.php
+++ application/libraries/GlobalMail.php
@@ -23,9 +23,9 @@ class GlobalMail{
 		$this->mail->IsSMTP();
 		$this->mail->CharSet = 'UTF-8';
 
-		$this->mail->Host       = "mail.research-academy.org"; // SMTP server 
-		$this->mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+		$this->mail->Host       = "webmail.research-academy.org"; // SMTP server 
 		$this->mail->SMTPAuth   = true;                  // enable SMTP authentication
+		$this->mail->SMTPSecure = 'ssl';
 		$this->mail->Port       = 465;                    // set the SMTP port for the GMAIL server
 		$this->mail->Username   = "testmail@research-academy.org"; // SMTP account username example
 		$this->mail->Password   = "goplay1212**";        // SMTP account password example
@@ -33,27 +33,28 @@ class GlobalMail{
 
 	
 	public function simpleMail($arrTo, $subject, $body){
-		try{
 
-			// penerima dan pengirim
-			$this->mail->setFrom($this->mail->Username, 'Mailer');
+		// penerima dan pengirim
+		$this->mail->setFrom($this->mail->Username, 'Mailer');
 
-			foreach ($arrTo as $value) {
-				$this->mail->addAddress($value);   
-			}
-
-			// content
-			// $this->mail->isHTML(true);                                  // Set email format to HTML
-		    $this->mail->Subject = $subject;
-		    $this->mail->Body    = $body;
-
-		    // send
-		    $this->mail->send();
-
-			return true;
-		}catch (Exception $e){
-			return false;
+		foreach ($arrTo as $value) {
+			$this->mail->addAddress($value);   
 		}
+
+		// content
+		// $this->mail->isHTML(true);                                  // Set email format to HTML
+	    $this->mail->Subject = $subject;
+	    $this->mail->Body    = $body;
+
+	    // send
+	    if(!$this->mail->send()){
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        }else{
+            return true;
+        }
+
+		
 	}
 
 }
