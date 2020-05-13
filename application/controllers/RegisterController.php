6e255669c4f8f4dbc4ba5fdc30891bc5c1907737
Tatag Winaryo <aku4layy@gmail.com>
2020-05-13 18:57:34 +0700

    fixing syntax 1

--- application/controllers/RegisterController.php
+++ application/controllers/RegisterController.php
@@ -93,6 +93,7 @@ class RegisterController extends CI_Controller
 	public function testMail(){
 
 		$arrTo = array("aku4layy@gmail.com");
+		$arrAttachment = array();
 		$subject = "Ini Subject";
 		$body = "Ini Body...";
 		$tesmail = $this->globalmail->simpleMail($arrTo, $arrAttachment, $subject, $body);
